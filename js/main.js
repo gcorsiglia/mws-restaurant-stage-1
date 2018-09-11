a3cb30d9e5da5f9e00e5493128ca18be1033c303
Gina Corsiglia <gina@ginasaysyes.com>
2018-09-11 10:26:32 -0700

    Accessibility added

--- js/main.js
+++ js/main.js
@@ -161,9 +161,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + ' in ' + restaurant.neighborhood;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -178,6 +179,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabIndex = '3';
+  more.setAttribute('role', 'button');
   li.append(more)
 
   return li
