de52b90d564527966423e5979db8a9339bc631aa
Gina Corsiglia <gina@ginasaysyes.com>
2018-09-12 18:49:27 -0700

    Initial cache working

--- sw.js
+++ sw.js
@@ -1,26 +1,34 @@
-/**
-let cacheID = 'fend-restaurant-001';
+/** Set Up Cache **/
+const cacheFiles = [
+	'/',
+	'/index.html',
+	'/restaurant.html',
+	'/css/styles.css',
+	'/css/reset.css',
+	'/data/restaurants.json',
+	'/js/main.js',
+	'/js/restaurant_info.js',
+	'/js/dbhelper.js',
+	'/img/1.jpg',
+	'/img/2.jpg',
+	'/img/3.jpg',
+	'/img/4.jpg',
+	'/img/5.jpg',
+	'/img/6.jpg',
+	'/img/7.jpg',
+	'/img/8.jpg',
+	'/img/9.jpg',
+	'/img/10.jpg'
+];
 
 self.addEventListener('install', event => {
 	event.waitUntil(
-		caches.open(cacheID)
+		caches.open('fend-restaurant-001')
 		.then(cache => {
-			return cache
-			.addAll([
-				'/',
-				'/index.html',
-				'/restaurant.html',
-				'/css/styles.css',
-				'/data/restaurants.json',
-				'/js/main.js',
-				'/js/restaurant_info.js',
-				'/js/register.js',
-				'/js/dbhelper.js'
-			])
+			return cache.addAll(cacheFiles)
 			.catch(error => {
 				console.log('Caches open failed ' + error);
 			});
 		})
-	)
+	);
 })
-**/
