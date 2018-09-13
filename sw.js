4189ad34e524cd61cd09900bab94e5349649ac3e
Gina Corsiglia <gina@ginasaysyes.com>
2018-09-12 20:00:22 -0700

    Service worker complete

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 /** Set Up Cache **/
+let cacheName = 'fend-restaurant-01';
 const cacheFiles = [
 	'/',
 	'/index.html',
@@ -22,13 +23,73 @@ const cacheFiles = [
 ];
 
 self.addEventListener('install', event => {
+	console.log('[ServiceWorker] Installed');
+
 	event.waitUntil(
-		caches.open('fend-restaurant-001')
+		caches.open(cacheName)
 		.then(cache => {
+			console.log('[ServiceWorker] Caching cacheFiles');
+
 			return cache.addAll(cacheFiles)
+			
 			.catch(error => {
-				console.log('Caches open failed ' + error);
+				console.log('[ServiceWorker] Caches open failed ' + error);
 			});
 		})
 	);
+});
+
+self.addEventListener('activate', event => {
+	console.log('[ServiceWorker] Activated');
+
+	event.waitUntil(
+		caches.keys().then(cacheNames => {
+			return Promise.all(cacheNames.map(thisCacheName => {
+
+				if (thisCacheName !== cacheName) {
+					console.log('[ServiceWorker] Removing cached files from ', thisCacheName);
+
+					return caches.delete(thisCacheName);
+				}
+			}));
+		})
+		.catch(error => {
+			console.log('[ServiceWorker] Failed to delete ' + thisCacheName + error);
+		})
+	);
+});
+
+
+self.addEventListener('fetch', event => {
+	console.log('[ServiceWorker] Fetching', event.request.url);
+
+	event.respondWith(
+		caches.match(event.request).then(response => {
+			if (response) {
+				console.log('[ServiceWorker] Found in cache: ', event.request.url);
+				
+				return response;
+			
+			} else {
+				console.log('[ServiceWorker] Could not find in cache. Fetching!! ', event.request.url);
+				
+				return fetch(event.request)
+				.then(response => {
+					console.log('[ServiceWorker] Adding request to cache ', event.request.url);
+
+					const clonedResponse = response.clone();
+
+					caches.open('cacheName').then(cache => {
+						cache.put(event.request, clonedResponse);
+					})
+
+					return response;
+				})
+				.catch(error => {
+					console.log('[ServiceWorker] Fetching failed ' + error);
+				})
+			}
+		})
+	);
 })
+
